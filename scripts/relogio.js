18edba098237199f6edb1dbc402bd57e687db375
Gabriel <gabrields1910@gmail.com>
2021-12-14 21:31:32 -0300

    Inicio da criação do relógio

--- scripts/relogio.js
+++ scripts/relogio.js
@@ -1,1 +1,7 @@
-console.log('relogio')
+const horaEl = document.getElementById('barra-tarefas__hora')
+const dataEl = document.getElementById('barra-tarefas__data')
+
+const zeroFill = (num) => {
+    return num < 10 ? '0' + num : num
+} 
+
